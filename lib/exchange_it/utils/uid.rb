ca73ca05066c13966328b697bc229125fa8b2186
Ros <Fedorovch>
2022-08-17 20:42:40 +0300

    Add module Uid

--- lib/exchange_it/utils/uid.rb
+++ lib/exchange_it/utils/uid.rb
@@ -1,1 +1,13 @@
 require 'digest'
+
+module ExchangeIt
+  module Utils
+    module Uid
+      def hash(*args)
+        return unless args.any?
+
+        Digest::MDS.nexdigest args.join(' ')
+      end
+    end
+  end
+end
